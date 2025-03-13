5e676b5b34662e9897d2ce392238667bc87692b8
D4r3N <enzo.suares@icloud.com>
2025-03-13 17:21:37 +0100

    Import other project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿SQLManager databaseManager = new SQLManager();
+int player_id = 0;
+        
+Console.WriteLine("Bienvenue sur Battleship X Pebble Dabble");
+Console.WriteLine("[1] - Se connecter");
+Console.WriteLine("[2] - S'inscrire");
+string input = Console.ReadLine();
+
+if (input == "1")
+{
+    Console.WriteLine("Entrer votre nom :");
+    string name = Console.ReadLine() ?? "Xx_Graou_xX";
+    Console.WriteLine("Entrer votre email :");
+    string email = Console.ReadLine() ?? "default@email.com";
+
+    player_id = databaseManager.GetPlayerId(name, email);
+} 
+else if (input == "2")
+{
+    Console.WriteLine("Entrer votre nom :");
+    string nom = Console.ReadLine() ?? "Xx_Graou_xX";
+    Console.WriteLine("Entrer votre age :");
+    int age = int.Parse(Console.ReadLine() ?? "20");
+    Console.WriteLine("Entrer votre email :");
+    string email = Console.ReadLine() ?? "default@email.com";
+            
+    player_id = databaseManager.AddNewPlayer(nom, age, email);
+}
+else
+{
+    Console.WriteLine("Tu te crois malin à avoir rentré autre chose que 1 ou 2 ? C'est pour tester le code ? Bah il n’y a pas de boucle si tu trompes, cheh, relance l'app");
+    Console.ReadKey();
+}
+
+PlayerManager playerManager = new PlayerManager(player_id);
+        
+databaseManager.CloseConnection();
---
+++ SQLManager.cs
@@ -0,0 +1,96 @@
+﻿using MySql.Data.MySqlClient;
+
+public class SQLManager
+{
+    private string server = "81.1.20.23";
+    private string port = "3306";
+    private string database = "USRS6N_1";
+    private string user = "EtudiantJvd";
+    private string password = "!?CnamNAQ01?!";
+    
+    private string connectionString;
+    private MySqlConnection connection;
+
+    public SQLManager()
+    {
+        connectionString = $"server={server};port={port};uid={user};pwd={password};database={database};";
+
+        try
+        {
+            connection = new MySqlConnection(connectionString);
+            Console.WriteLine("Connecting to the Server...");
+            connection.Open();
+        }
+        catch (MySqlException exeption)
+        {
+            Console.WriteLine(exeption.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Sucessfully connected to the Server");
+        }
+    }
+    
+    public void CloseConnection()
+    {
+        connection.Close();
+    }
+
+    public int AddNewPlayer(string name, int age, string email)
+    {
+        try
+        {
+            string query = "INSERT INTO Gr5_joueur (nom, age, mail, victoire, date_inscription) VALUES (@name, @age, @mail, 0, CURDATE());";
+            
+            MySqlCommand command = new MySqlCommand(query, connection);
+            
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@age", age);
+            command.Parameters.AddWithValue("@mail", email);
+            
+            int rowsAffected = command.ExecuteNonQuery();
+            Console.WriteLine("Row affected " + rowsAffected);
+        }
+        catch (MySqlException exeption)
+        {
+            Console.WriteLine(exeption.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Your account has been sucessfully created");
+        }
+        
+        return GetPlayerId(name, email);
+    }
+
+    public int GetPlayerId(string name, string email)
+    {
+        string safeName = MySqlHelper.EscapeString(name);
+        string safeEmail = MySqlHelper.EscapeString(email);
+
+        string query = $"SELECT joueur_id FROM Gr5_joueur WHERE nom = '{safeName}' AND mail = '{safeEmail}';";
+        MySqlDataReader dataReader = ExecuteReaderQuery(query);
+        
+        dataReader.Read();
+        int player_id = dataReader.GetInt32(0);
+        dataReader.Close();
+        
+        return player_id;
+    }
+    
+    private MySqlDataReader ExecuteReaderQuery(string query)
+    {
+        MySqlDataReader reader = null;
+        try
+        {
+            MySqlCommand command = new MySqlCommand(query, connection);
+            reader = command.ExecuteReader();
+        }
+        catch (MySqlException exeption)
+        {
+            Console.WriteLine(exeption.Message);
+        }
+        
+        return reader;
+    }
+}
